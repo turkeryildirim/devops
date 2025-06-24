e92e373f2626d6fad6743dfd93f8d8dc064383ea
Türker YILDIRIM <me@turkeryildirim.com>
2025-06-24 17:13:09 +0300

    test

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -8,15 +8,16 @@ use PHPUnit\Framework\TestCase;
 
 class SampleTest extends TestCase
 {
+    private int $data;
+
     protected function setUp(): void
     {
         parent::setUp();
+        $this->data = 10 / 2;
     }
 
     public function testSample(): void
     {
-        $data = null;
-
-        $this->assertNull($data);
+        $this->assertEquals(5, $this->data);
     }
 }
